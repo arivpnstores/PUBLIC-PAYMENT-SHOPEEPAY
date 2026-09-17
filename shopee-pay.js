e51665ccd2df70f9e984dbc9b0c64e342bafe71f
PT RAJA SERVER PREMIUM <arivpnstores@gmail.com>
2026-09-17 14:04:36 +0700

    ip

--- shopee-pay.js
+++ shopee-pay.js
@@ -4,6 +4,7 @@ function getBaseUrl(vars) {
   return (vars && vars.SHOPEEPAY_BASE_URL) || 'http://localhost:2007';
 }
 
+async function createQRIS(amount, vars) {
   const base = getBaseUrl(vars);
   const res = await axios.get(
     `${base}/createqris/amount=${encodeURIComponent(Number(amount))}`,
@@ -18,6 +19,7 @@ async function checkPayment(orderSn, vars) {
     `${base}/cekpembayaran/transaction_sn=${encodeURIComponent(String(orderSn))}`,
     { timeout: 15000 }
   );
+  return res.data;
 }
 
 module.exports = { createQRIS, checkPayment, getBaseUrl };
